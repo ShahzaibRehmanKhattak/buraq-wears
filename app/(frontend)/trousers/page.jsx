f645e5ba5afd7ac751e61b5c534929f124365a9c
Shahzaib Rehman <shahzaibkhattak0319@gmail.com>
2026-05-15 08:03:24 -0700

    added tousers page

---
+++ app/(frontend)/trousers/page.jsx
@@ -0,0 +1,7 @@
+export default function Trousers() {
+    return (
+      <div className="antialiased mt-15 selection:bg-black selection:text-white bg-[#f9f9f9]">
+        hello trousers
+      </div>
+    );
+  }
